946951466621f008d5aa1da9535dbd668fd536d3
Elizabeth_Chernova <elizabethchernova@gmail.com>
2023-09-11 16:46:20 +0300

    import

--- app/src/test/java/edu/kmaooad/capstone23/jobs/controllers/ActivateJobControllerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/jobs/controllers/ActivateJobControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import edu.kmaooad.capstone23.jobs.controllers.*;
-
+import edu.kmaooad.capstone23.jobs.commands.*;
 class ActivateJobControllerTest {
 
 
